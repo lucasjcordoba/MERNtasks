1428d6d177a667b0d4ebd4f85204100e1dd8ecca
lucasjcordoba <lucasjcordoba@gmail.com>
2021-02-20 04:25:45 -0300

    first steps

---
+++ src/components/auth/Login.js
@@ -0,0 +1,85 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const Login = () => {
+
+    // State para iniciar sesión
+    const [usuario, guardarUsuario] = useState({
+        email: '',
+        password: ''
+    });
+
+    // Extraer usuario
+    const { email, password } = usuario
+
+    const onChange = e => {
+        guardarUsuario({
+            ...usuario,
+            [e.target.name] : e.target.value
+        })
+    }
+
+    // Cuando el usuario quiere iniciar sesión
+    const onSubmit = e => {
+        e.preventDefault();
+
+        // Validar que no haya campos vacíos
+
+        // Pasarlo al action
+    }
+
+    return ( 
+        <div className="form-usuario">
+            <div className="contenedor-form sombra-dark">
+                <h1>Iniciar Sesión</h1>
+
+                <form
+                    unSubmit={onSubmit}
+                >
+                    <div className="campo-form">
+                        <label htmlFor="email">Email</label>
+                        <input 
+                            type="email"
+                            id="email"
+                            name="email"
+                            value={email}
+                            placeholder="Tu Email"
+                            onChange={onChange}
+                        />    
+
+                    </div>
+
+                    <div className="campo-form">
+                        <label htmlFor="password">Password</label>
+                        <input 
+                            type="password"
+                            id="password"
+                            name="password"
+                            value={password}
+                            placeholder="Tu Password"
+                            onChange={onChange}
+                        />    
+
+                    </div>
+
+                    <div className="campo-form">
+                        <input 
+                            type="submit"
+                            className="btn btn-primario btn-block"
+                            value="Iniciar Sesión"
+                        />
+
+                    </div>
+                </form>
+
+                <Link to={'/nueva-cuenta'} className="clase-cuenta" >
+                    Obtener Cuenta
+                </Link>
+
+            </div>
+
+        </div>
+     );
+}
+ 
+export default Login;
